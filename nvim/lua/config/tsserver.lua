343f0a28e2d50a39d9a882c3b77d5cc829a05ae1
SconeForScale <scone.mail.sh@gmail.com>
2024-08-26 01:51:33 -0700

    add js lsp config

---
+++ nvim/lua/config/tsserver.lua
@@ -0,0 +1,10 @@
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("lspconfig").tsserver.setup({
+    capabilities = capabilities,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        }
+    }
+})
