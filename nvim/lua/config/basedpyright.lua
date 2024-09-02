c8a5536ebd99024619fa904a00f5ada91080546d
SconeForScale <scone.mail.sh@gmail.com>
2024-09-02 09:00:22 -0700

    fix basedpyright pythonpath

--- nvim/lua/config/basedpyright.lua
+++ nvim/lua/config/basedpyright.lua
@@ -1,7 +1,17 @@
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-
 require("lspconfig").basedpyright.setup({
     capabilities = capabilities,
+    before_init = function(_, config)
+        local venv_path = vim.fn.getcwd().."/.venv/bin/python"
+        config.settings.python = {}
+        if vim.fn.filereadable(venv_path) == 1 then
+            config.settings.python.pythonPath = venv_path
+        else
+            config.settings.python.pythonPath = "/usr/bin/python"
+        end
+    end,
 })
+
+
